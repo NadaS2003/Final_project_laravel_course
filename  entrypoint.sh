9af7a029390b27996191c2b7255143012a497421
Nada Salama <nadaramadan1512003@gmail.com>
2025-06-19 13:00:35 +0300

    Add missing entrypoint.sh script

---  entrypoint.sh
+++  entrypoint.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
-# توليد المفتاح إذا مش موجود
+# تأكد من وجود APP_KEY
+if [ ! -f .env ]; then
+  cp .env.example .env
+fi
+
+# توليد مفتاح التطبيق إذا غير موجود
+php artisan key:generate
+
+# تنفيذ الترحيلات
+php artisan migrate --force
+
+# توليد مفاتيح Passport إذا غير موجودة
 if [ ! -f storage/oauth-private.key ]; then
-  echo "Generating passport keys..."
-  php artisan migrate --force
+  echo "Generating Passport keys..."
   php artisan passport:keys
 fi
 
-# تشغيل Laravel
+# تشغيل السيرفر
 php artisan serve --host=0.0.0.0 --port=8000
